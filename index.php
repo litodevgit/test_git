98e37a7089050eacb1d59380e1f868a0d4aa19c7
issackTodevweb <emmanuelitorandria@gmail.com>
2025-08-22 18:39:00 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+</head>
+<body>
+    <h1>Bienvenue sur la page d'accueil</h1>
+    <p>Ceci est un exemple de page d'accueil.</p>
+</body>
+</html>
