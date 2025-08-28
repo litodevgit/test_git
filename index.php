a4c9ad07ee72bf0c3b70437a7812e2c31c0b0462
litodevgit <litomanto2@gmail.com>
2025-08-28 13:05:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <h1>Bienvenue sur la page d'accueil</h1>
     <p>Ceci est un exemple de page d'accueil.</p>
     <p>ceci est mon modification (IssackTo dev)</p>
+    <p>ceci est la derniere modification</p>
     
 </body>
 </html>
