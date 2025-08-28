96da54ddc5bab8a7bee17fb8bff88385a4deeb9e
litodevgit <litomanto2@gmail.com>
2025-08-28 11:13:17 +0100

    Update stok.php

--- stok.php
+++ stok.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+</head>
+<body>
+    <h1>j'ai ajouter ce fichier aussi </h1>
+    
+</body>
+</html>
